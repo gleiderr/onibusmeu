7f46cdf46396bc7ab7efa027812d68faff3f27f2
Gleider Ribeiro Costa <gleiderr@gmail.com>
2019-02-14 12:31:15 -0200

    Aplicação de Bootstrap CSS;

--- src/index.js
+++ src/index.js
@@ -4,64 +4,23 @@ import './index.css';
 //import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-const linhas = [
-  {
-    nome: '5800',
-    horários: [
-      {
-        variação: 'Dia Útil',
-        partidas: [
-          { hora: '00:30', local: 'Terminal Morro Alto'},
-          { hora: '01:30', local: 'Terminal Morro Alto'},
-          { hora: '02:30', local: 'Terminal Morro Alto'},
-          { hora: '03:30', local: 'Terminal Morro Alto'},
-          { hora: '00:30', local: 'Terminal Morro Alto'},
-          { hora: '00:30', local: 'Terminal Morro Alto'},
-          { hora: '00:30', local: 'Terminal Morro Alto'},
-        ],
-      },
-      {
-        variação: 'Sábado',
-        partidas: [
-          { hora: '00:30', local: 'Terminal Morro Alto'},
-          { hora: '01:30', local: 'Terminal Morro Alto'},
-          { hora: '02:30', local: 'Terminal Morro Alto'},
-          { hora: '03:30', local: 'Terminal Morro Alto'},
-          { hora: '00:30', local: 'Terminal Morro Alto'},
-          { hora: '00:30', local: 'Terminal Morro Alto'},
-          { hora: '00:30', local: 'Terminal Morro Alto'},
-        ],
-      },
-      {
-        variação: 'Domingos e Feriados',
-        partidas: [
-          { hora: '00:30', local: 'Terminal Morro Alto'},
-          { hora: '01:30', local: 'Terminal Morro Alto'},
-          { hora: '02:30', local: 'Terminal Morro Alto'},
-          { hora: '03:30', local: 'Terminal Morro Alto'},
-          { hora: '00:30', local: 'Terminal Morro Alto'},
-          { hora: '00:30', local: 'Terminal Morro Alto'},
-          { hora: '00:30', local: 'Terminal Morro Alto'},
-        ],
-      },
-    ]
-  },
-];
-
 function Horario(props) {
   const partidas = props.partidas.map((partida, index) => {
       return (
-        <div key={index}>
-          <strong>- {partida.hora} </strong>
-          <small>{partida.local}</small>
-        </div>
+        <span className='border rounded m-1' key={index}>
+          {partida.hora} 
+          {/*<small>
+            {partida.inicio ? ' (Partida: ' + partida.inicio + ')': ''}
+            {partida.termino ? ' (Recolhimento: ' + partida.termino + ')': ''}
+          </small>*/}
+        </span>
       );
     });
 
   return (
     <div>
-      <h3>{props.variação}</h3>
-      {partidas}
+      <h6>{props.variação}</h6>
+      <div className='d-flex flex-wrap'>{partidas}</div>
     </div>
    );
 }
@@ -74,9 +33,12 @@ function Linha(props) {
   });
 
   return (
-    <div>
-      <h2>{props.nome}</h2>
-      {horários}
+    <div className='card card-body'>
+      <h5 className='card-title mb-0'>{props.nome}</h5>
+      <div className='card-text'>
+        {/*<div><strong>Partida padrão:</strong> {props.partidaPadrão}</div>*/}
+        {horários}
+      </div>
     </div>
    );
 }
@@ -84,14 +46,178 @@ function Linha(props) {
 function Linhas(props) {
   const linhas = props.linhas.map((linha) => {
     return (<Linha key={linha.nome} 
-                   nome={linha.nome} 
+                   nome={linha.nome}
+                   partidaPadrão={linha.partidaPadrão}
                    horários={linha.horários} />);
   })
 
   return (<div>{linhas}</div>);
 }
 
-ReactDOM.render(<Linhas linhas={linhas}/>, document.getElementById('root'));
+function HomePage(props) {
+  const linhas = [
+    {
+      nome: '5800 - Caieiras',
+      partidaPadrão: 'Terminal Morro Alto',
+      horários: [
+        {
+          variação: 'Dia Útil',
+          partidas: [
+            { hora: '00:30'},
+            { hora: '01:50'},
+            { hora: '03:10'},
+            { hora: '04:00', inicio:'Rua Paulo Ferreira da Costa'},
+            { hora: '04:30'},
+            { hora: '04:40'},
+            { hora: '04:50', inicio:'Rua Paulo Ferreira da Costa'},
+            { hora: '05:00', inicio:'Rua Paulo Ferreira da Costa'},
+            { hora: '05:10', inicio:'Rua Paulo Ferreira da Costa'},
+            { hora: '05:30'}, { hora: '05:45'}, { hora: '06:00'}, { hora: '06:20'},
+            { hora: '06:35'}, { hora: '06:50'}, { hora: '07:35'}, { hora: '07:55'},
+            { hora: '08:15'}, { hora: '08:45'}, { hora: '09:15'}, { hora: '09:45'},
+            { hora: '10:15'}, { hora: '10:45'}, { hora: '11:15'}, { hora: '11:45'},
+            { hora: '12:25'}, { hora: '12:45'}, { hora: '13:10'}, 
+            { hora: '13:25'},
+            { hora: '13:45'},
+            { hora: '14:05'},
+            { hora: '14:25'},
+            { hora: '14:45'},
+            { hora: '15:05'},
+            { hora: '15:20'},
+            { hora: '15:35'},
+            { hora: '15:50'},
+            { hora: '16:10'},
+            { hora: '16:20'},
+            { hora: '16:30'},
+            { hora: '16:50'},
+            { hora: '17:00'},
+            { hora: '17:20'},
+            { hora: '17:35'},
+            { hora: '17:50'},
+            { hora: '18:05'},
+            { hora: '18:20'},
+            { hora: '18:35', termino:'Rua Paulo Ferreira da Costa'},
+            { hora: '18:50'},
+            { hora: '19:10'},
+            { hora: '19:30'},
+            { hora: '19:50'},
+            { hora: '20:10'},
+            { hora: '20:30'},
+            { hora: '20:50', termino:'Rua Paulo Ferreira da Costa'},
+            { hora: '21:25'},
+            { hora: '22:00'},
+            { hora: '22:35'},
+            { hora: '23:10', termino:'Rua Paulo Ferreira da Costa'},
+            { hora: '23:45', termino:'Rua Paulo Ferreira da Costa'},
+          ],
+        },
+        {
+          variação: 'Sábado',
+          partidas: [
+            { hora: '00:30'},
+            { hora: '01:50'},
+            { hora: '03:10'},
+            { hora: '05:05'},
+            { hora: '05:35'},
+            { hora: '06:20'},
+            { hora: '06:40'},
+            { hora: '07:05'},
+            { hora: '07:35'},
+            { hora: '08:25'},
+            { hora: '08:45'},
+            { hora: '09:05'},
+            { hora: '09:35'},
+            { hora: '10:05'},
+            { hora: '10:35'},
+            { hora: '11:05'},
+            { hora: '11:55'},
+            { hora: '12:35'},
+            { hora: '13:15'},
+            { hora: '13:55'},
+            { hora: '14:35'},
+            { hora: '15:15'},
+            { hora: '15:55'},
+            { hora: '16:35'},
+            { hora: '17:15'},
+            { hora: '17:55'},
+            { hora: '18:20'},
+            { hora: '18:45'},
+            { hora: '19:10', termino:'Rua Paulo Ferreira da Costa'},
+            { hora: '19:35'},
+            { hora: '20:05'},
+            { hora: '20:35'},
+            { hora: '21:25'},
+            { hora: '22:25'},
+            { hora: '23:10', termino:'Rua Paulo Ferreira da Costa'},
+            { hora: '23:45', termino:'Rua Paulo Ferreira da Costa'},
+          ],
+        },
+        {
+          variação: 'Domingos e Feriados',
+          partidas: [
+            { hora: '00:30'},
+            { hora: '01:50'},
+            { hora: '03:10'},
+            { hora: '04:30'},
+            { hora: '05:00'},
+            { hora: '05:30'},
+            { hora: '06:00'},
+            { hora: '06:30'},
+            { hora: '07:00'},
+            { hora: '07:45'},
+            { hora: '08:45'},
+            { hora: '09:45'},
+            { hora: '10:55'},
+            { hora: '11:55'},
+            { hora: '12:55'},
+            { hora: '13:55'},
+            { hora: '14:55'},
+            { hora: '15:55'},
+            { hora: '16:55'},
+            { hora: '17:55', termino:'Rua Paulo Ferreira da Costa'},
+            { hora: '18:55'},
+            { hora: '19:55'},
+            { hora: '20:55'},
+            { hora: '22:15'},
+            { hora: '23:25', termino:'Rua Paulo Ferreira da Costa'},
+          ],
+        },
+      ]
+    },
+  ];
+
+  return (
+    <div className='h-100'>
+      <header>
+        <nav>
+          <div className='navbar navbar-expand-lg navbar-dark bg-primary'>
+            <div className='navbar-brand'>
+              <span role='img' aria-label='Ô'>🚍</span>
+              nibus Meu
+            </div>
+          </div>
+        </nav>
+      </header>
+      <main className='container my-2'>
+        <Linhas linhas={linhas} />
+      </main>
+      <footer className='bg-primary' >
+        <div className='container text-light text-center'>
+        {'Desenvolvido por '}
+        <a className='text-light' 
+           href='https://gleiderr.github.io' 
+           rel='noopener noreferrer' 
+           target='_blank' 
+           style={{'textDecoration': 'underline'}}>
+          Gleider Costa
+        </a>
+        </div>
+      </footer>
+    </div>
+  );
+}
+
+ReactDOM.render(<HomePage />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
