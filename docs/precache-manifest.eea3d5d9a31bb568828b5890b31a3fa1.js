608a55c49a16cdfc895531e575b889ae5cc82973
Gleider Ribeiro Costa <gleiderr@gmail.com>
2019-02-16 09:43:52 -0200

    Preparação para subir primeira versão em produção.

---
+++ docs/precache-manifest.eea3d5d9a31bb568828b5890b31a3fa1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "eebbb8981480f13eeaaf",
+    "url": "/onibusmeu/static/js/runtime~main.eebbb898.js"
+  },
+  {
+    "revision": "52bb60d60f13a52ce676",
+    "url": "/onibusmeu/static/js/main.52bb60d6.chunk.js"
+  },
+  {
+    "revision": "991e61a56c503e80f2d2",
+    "url": "/onibusmeu/static/js/2.991e61a5.chunk.js"
+  },
+  {
+    "revision": "52bb60d60f13a52ce676",
+    "url": "/onibusmeu/static/css/main.be9cd952.chunk.css"
+  },
+  {
+    "revision": "92dbc64b3077ff4ad8aad1917f5864f4",
+    "url": "/onibusmeu/index.html"
+  }
+];
